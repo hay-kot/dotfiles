a632e6d574f93435057c53fb3db6f9d832f36154
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-11-06 11:23:39 -0900

    fix(auto install packer):
    auto install packer using example from repository

--- .config/nvim/lua/haykot/plugins.lua
+++ .config/nvim/lua/haykot/plugins.lua
@@ -23,6 +23,20 @@ vim.cmd [[
   augroup end
 ]]
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
