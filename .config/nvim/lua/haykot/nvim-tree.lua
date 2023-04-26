d05c644dd0595d66e446780a1d88f3393c728865
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-04-26 11:08:04 -0800

    use centered window for file tree / explorer

--- .config/nvim/lua/haykot/nvim-tree.lua
+++ .config/nvim/lua/haykot/nvim-tree.lua
@@ -10,6 +10,9 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.6  -- You can change this too
+
 nvim_tree.setup({
   git = {
     ignore = true,
@@ -57,15 +60,40 @@ nvim_tree.setup({
     },
   },
   view = {
-    width = 30,
     side = "left",
     mappings = {
       list = {
         { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
         { key = "h",                  cb = tree_cb("close_node") },
         { key = "v",                  cb = tree_cb("vsplit") },
+        -- <leader><leader> to open tree
+        { key = "<leader><leader>", cb = tree_cb("expand") },
       },
     },
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+        return {
+          border = "rounded",
+          relative = "editor",
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+      end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
   },
   actions = {
     open_file = {
