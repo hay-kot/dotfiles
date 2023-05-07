32b3e0d06477d784ae3cd5a3ba33797a06cdcdb0
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-05-07 13:53:38 -0800

    fix globals and wrap text for markdown

---
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+vim.cmd([[
+augroup Markdown
+  autocmd!
+  autocmd FileType markdown set wrap
+augroup END
+set wrap
+]])
--- .config/nvim/lua/haykot/lib/utils.lua
+++ .config/nvim/lua/haykot/lib/utils.lua
@@ -1,32 +1,33 @@
-M = {}
-
 --- file_exists checks if a file exists. Returns true if it does, false otherwise
 -- @param name string: The name of the file
-M.file_exists = function(name)
+local function file_exists(name)
   local f = io.open(name, "r")
   return f ~= nil and io.close(f)
 end
 
---- find_first searches for the first file in a list of files in a given directory
--- @param root string: The root directory to search in
--- @param files table: A table of strings containing the names of the files to search for
--- @return string: The path to the first file found
-M.find_first = function(root, files)
-  -- FIles is table of strings
-  for _, file in pairs(files) do
-    if M.file_exists(root .. "/" .. file) then
-      return root .. "/" .. file
+M = {
+  file_exists = file_exists,
+  --- find_first searches for the first file in a list of files in a given directory
+  -- @param root string: The root directory to search in
+  -- @param files table: A table of strings containing the names of the files to search for
+  -- @return string: The path to the first file found
+  find_first = function(root, files)
+    -- FIles is table of strings
+    for _, file in pairs(files) do
+      if file_exists(root .. "/" .. file) then
+        return root .. "/" .. file
+      end
     end
-  end
-end
+  end,
 
-M.guard_module = function(modules)
-  for _, module in pairs(modules) do
-    if not pcall(require, module) then
-      return false
+  guard_module = function(modules)
+    for _, module in pairs(modules) do
+      if not pcall(require, module) then
+        return false
+      end
     end
-  end
-  return true
-end
+    return true
+  end,
+}
 
 return M
