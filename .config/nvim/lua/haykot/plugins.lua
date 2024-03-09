053658ac0844defd6f8bd937fae1458116d33b8a
Hayden <64056131+hay-kot@users.noreply.github.com>
2024-03-09 11:27:06 -0600

    drop ray-x/go.nvim

--- .config/nvim/lua/haykot/plugins.lua
+++ .config/nvim/lua/haykot/plugins.lua
@@ -236,21 +236,21 @@ require("lazy").setup({
   },
 
   -- Go
-  {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-    keys = {
-      { "<leader>er", ":GoIfErr<cr>", desc = "insert err return" },
-    },
-  },
+  --[[ { ]]
+  --[[   "ray-x/go.nvim", ]]
+  --[[   dependencies = { -- optional packages ]]
+  --[[     "ray-x/guihua.lua", ]]
+  --[[     "neovim/nvim-lspconfig", ]]
+  --[[     "nvim-treesitter/nvim-treesitter", ]]
+  --[[   }, ]]
+  --[[   config = function() ]]
+  --[[     require("go").setup() ]]
+  --[[   end, ]]
+  --[[   event = { "CmdlineEnter" }, ]]
+  --[[   ft = { "go", "gomod" }, ]]
+  --[[   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries ]]
+  --[[   keys = { ]]
+  --[[     { "<leader>er", ":GoIfErr<cr>", desc = "insert err return" }, ]]
+  --[[   }, ]]
+  --[[ }, ]]
 })
