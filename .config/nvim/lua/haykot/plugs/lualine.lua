d9620b457bdf772a174557b424305c3025ebc73b
Hayden <64056131+hay-kot@users.noreply.github.com>
2025-06-26 15:28:52 -0500

    align themeing

--- .config/nvim/lua/haykot/plugs/lualine.lua
+++ .config/nvim/lua/haykot/plugs/lualine.lua
@@ -38,30 +38,8 @@ return {
         return vim.bo.filetype
       end
 
-      local colors = {
-        fg = "#76787d",
-        bg = "#252829",
-      }
       require("lualine").setup({
-        options = {
-          theme = {
-            normal = {
-              a = { fg = colors.fg, bg = colors.bg },
-              b = { fg = colors.fg, bg = colors.bg },
-              c = { fg = colors.fg, bg = colors.bg },
-            },
-            insert = { a = { fg = colors.fg, bg = colors.bg }, b = { fg = colors.fg, bg = colors.bg } },
-            visual = { a = { fg = colors.fg, bg = colors.bg }, b = { fg = colors.fg, bg = colors.bg } },
-            command = { a = { fg = colors.fg, bg = colors.bg }, b = { fg = colors.fg, bg = colors.bg } },
-            replace = { a = { fg = colors.fg, bg = colors.bg }, b = { fg = colors.fg, bg = colors.bg } },
-
-            inactive = {
-              a = { bg = colors.fg, fg = colors.bg },
-              b = { bg = colors.fg, fg = colors.bg },
-              c = { bg = colors.fg, fg = colors.bg },
-            },
-          },
-        },
+        options = { theme = "tokyonight" },
         sections = {
           lualine_a = { "branch" },
           lualine_b = { { "filename", path = 1 } },
