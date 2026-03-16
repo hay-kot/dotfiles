ebca64f953fd6f05a42f5af14ea14baed4123a81
Hayden <64056131+hay-kot@users.noreply.github.com>
2026-03-16 16:38:27 -0500

    fix treesitter?

--- .config/nvim/lua/haykot/plugs/treesitter.lua
+++ .config/nvim/lua/haykot/plugs/treesitter.lua
@@ -2,13 +2,11 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   dependencies = {
-    -- "p00f/nvim-ts-rainbow",
-
     {
       "nvim-treesitter/nvim-treesitter-context",
       config = function()
         require("treesitter-context").setup({
-          enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+          enable = true,
         })
 
         vim.api.nvim_create_user_command("ToggleTreeSitterContext", function()
@@ -18,31 +16,16 @@ return {
     },
   },
   config = function()
-    local configs = require("nvim-treesitter.configs")
+    require("nvim-treesitter").setup()
 
-    configs.setup({
-      ensure_installed = "all",
-      ignore_install = { "phpdoc", "tlaplus", "ruby" },
-      highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { "css" }, -- list of language that will be disabled
-      },
-      autopairs = {
-        enable = true,
-      },
-      indent = { enable = true, disable = { "python", "css" } },
-      rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-      },
-      contet_commentstring = {
-        enabled = true,
-        enable_autocmd = false,
-      },
+    -- Enable treesitter highlighting and indentation
+    vim.treesitter.language.register("bash", "zsh")
+
+    -- Auto-install parsers when entering a buffer
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
     })
   end,
 }
